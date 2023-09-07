3356fb611995b2b784cb8ddbc0b472a47c07a6e1
aksingh002 <aksinghcool007@gmail.com>
2023-09-07 20:04:10 +0530

    simple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 
-
 @NgModule({
-    declarations:[AppComponent, BooksComponent],
-    imports:[BrowserModule],
-    bootstrap:[AppComponent]
+  declarations: [AppComponent , BooksComponent],
+  imports: [BrowserModule, FormsModule],
+  
+  bootstrap: [AppComponent]
 })
-export class AppModule{}
+export class AppModule { }
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface book {
+  name:string;
+  rate:number
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent {
-  name = 'karan singh'
+  name:string = 'karan singh'
+  src:string= ''
+  is:boolean=false
+
+  book:book[] = [{name:"this is asweme ",rate:200},{ name:" this is cool",rate:200}]
+
+ 
+  
 }
