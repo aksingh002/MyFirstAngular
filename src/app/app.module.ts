01a6488c91de3f8214b6b0fa1fe8e79793619cc2
aksingh002 <aksinghcool007@gmail.com>
2023-09-07 21:30:27 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
-  declarations: [AppComponent , BooksComponent],
+  declarations: [AppComponent , BooksComponent, BookComponent],
   imports: [BrowserModule, FormsModule],
-  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 interface book {
   name:string;
-  rate:number
+  rate:number;
+  image:string;
 }
 
 @Component({
@@ -15,7 +16,7 @@ export class BooksComponent {
   src:string= ''
   is:boolean=false
 
-  book:book[] = [{name:"this is asweme ",rate:200},{ name:" this is cool",rate:200}]
+  book:book[] = [{name:"this is asweme ",rate:200,image:"/assets/images/PXL_20230218_164053220.jpg"},{ name:" this is cool",rate:200,image:"/assets/images/karan_singh_image.png"}]
 
  
   
